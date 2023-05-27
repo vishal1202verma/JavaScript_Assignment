a3a91cd3e7639072a02e01332d0f3151c8ccca9f
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-05-28 00:36:26 +0530

    DOM project-2 commited

---
+++ DOMproject-2/Script.js
@@ -0,0 +1,7 @@
+const changeColor = document.getElementById("btn");
+changeColor.addEventListener("click", Addevent = ()=>{
+    changeColor.style.backgroundColor = "rgb(243, 34, 15)";
+    changeColor.style.color = "black";
+    changeColor.innerText = "logout !"; 
+    
+});
